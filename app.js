b072befda0273ca8e757bcda6bf4c657768d037d
Mikhail Tsoy <mixail0071@yandex.ru>
2022-06-24 14:09:06 +0200

    fix

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res
-    .status(statusCode)
+    .statusCode(statusCode)
     .send({
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
