d389327ca2bfd3702fa4e1d4fa2f5573304f5a7e
Mikhail Tsoy <mixail0071@yandex.ru>
2022-06-24 18:08:59 +0200

    fix

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~://?#[\]@!$&'()*+,;=.]+/im, { invert: true }),
+      avatar: Joi.string().regex(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~://?#[\]@!$&'()*+,;=.]+/im),
       // avatar: Joi.string().regex(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w
       // \.-]+)+[\w\-\._~:\/\/?#[\]@!\$&'\(\)\*\+,;=.]+/im, { invert: true }),
       email: Joi.string().required().email(),
